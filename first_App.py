449e0332a801340293e4e24a3e7d5bab2da22b41
sutharshan.venkatac <sutharshan.venkatac@wolterskluwer.com>
2023-11-20 18:18:37 +0530

    trail fro conflict

--- first_App.py
+++ first_App.py
@@ -1,5 +1,4 @@
 a = int(input("enter 1st number :"))
 b = int(input("enter 2nd number :"))
 c = int(input("enter 3rd number :"))
-print(f"Addition of two given values is : {a+b+c}")
-
+print(f"Addition of three given values is : {a+b+c}")
