4f204fc2f42858806c90fb922a945c4b216e8d84
sutharshan-venkatachalam <105276748+sutharshan-venkatachalam@users.noreply.github.com>
2023-11-20 18:09:24 +0530

    Update first_App.py
    var c added

--- first_App.py
+++ first_App.py
@@ -1,7 +1,5 @@
 a = int(input("enter 1st number :"))
 b = int(input("enter 2nd number :"))
+c = int(input("enter 3rd number :"))
+print(f"Addition of two given values is : {a+b+c}")
 
-print(f"Addition of two given values is : {a+b}")
-
-print("first sub branch")
-print("first sub branch")
